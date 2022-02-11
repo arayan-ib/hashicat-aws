d7126387ca2e86f230fdc703f6bd81bdd0de38cb
root <arayan@infoblox.com>
2022-02-11 00:57:43 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/infoblox-arayan/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket = "gaurav-s3-bucket"
+  bucket = "arayan-test-s3-bucket"
   acl    = "private"
   
 }
